058935d8fbaf8a7f6deaf5257f75ce9111898871
chatan000 <58610792+chatan000@users.noreply.github.com>
2020-02-27 13:25:18 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+// Write out a list of your five favorite foods here:
+
+// 1. pizza
+
+let food1 = "pizza"
+
+// 2. chicken
+
+let food2 = "chicken"
+
+// 3. steak
+
+let food3 = "steak"
+
+// 4. salad
+
+let food4 = "salad"
+
+// 5. sushi
+
+let food5 = "sushi"
+
+// Display all five favorite foods in a single array
+
+
+let listFoods = ["pizza", "steak"]
+
+// Display only every food ;isted. 1, 3, and 5.
+
+game.splash(listFoods[0])
+
+
+
+
